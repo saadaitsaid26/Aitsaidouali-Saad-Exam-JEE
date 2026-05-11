98031fd19a0569124fe99611399b9b46b43f804f
saadaitsaid26 <saadaitsaid26@gmail.com>
2026-05-11 09:05:53 +0100

    Step 0: Spring Boot scaffold - package structure, pom.xml, application.yml

---
+++ src/main/java/ma/enset/aitsaidouali/insurance/InsuranceManagementApplication.java
@@ -0,0 +1,11 @@
+package ma.enset.aitsaidouali.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceManagementApplication.class, args);
+    }
+}
